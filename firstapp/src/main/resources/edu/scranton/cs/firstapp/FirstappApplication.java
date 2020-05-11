79f27852b455e491a308e6c45af2fa2a76756098
LuisColina-Garcia <luis.colina-garcia@scranton.edu>
2020-05-10 20:33:16 -0400

    Added java classes to build app and modified pom.xml file

---
+++ firstapp/src/main/resources/edu/scranton/cs/firstapp/FirstappApplication.java
@@ -0,0 +1,19 @@
+package edu.scranton.cs.firstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class FirstappApplication {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+			return application.sources(FirstappApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstappApplication.class, args);
+	}
+}
